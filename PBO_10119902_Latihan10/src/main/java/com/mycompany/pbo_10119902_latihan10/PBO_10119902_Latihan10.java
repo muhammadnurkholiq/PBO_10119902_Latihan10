85fe180f3250abe06a170c8ff698d393b513214f
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-21 10:40:00 +0700

    Add files via upload
    First Commit

---
+++ PBO_10119902_Latihan10/src/main/java/com/mycompany/pbo_10119902_latihan10/PBO_10119902_Latihan10.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo_10119902_latihan10;
+
+/**
+ *
+ * @author Muhammad Nurkholiq
+ */
+public class PBO_10119902_Latihan10 {
+    public static void main(String[] args) {
+        int umur  = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : "+ dewasa);
+    }
+}
